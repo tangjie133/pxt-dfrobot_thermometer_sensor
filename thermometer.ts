097820ab168b33f4033283a84b494b6c1df1d3c7
TgJe <51323906+tangjie133@users.noreply.github.com>
2019-10-16 13:46:59 +0800

    Update pxt.json, main.blocks, thermometer.ts

--- thermometer.ts
+++ thermometer.ts
@@ -3,11 +3,15 @@ namespace DFRobot_Thermometer_Sensor{
     let i2cAddr=0x5A
     //%block="getobjecttemp_C"
     export function GetObjectTemp_C():number {
+        let temp
+        temp=i2cReadRegister()*0.02-273.15
+        return 0
+    }
+    function  i2cReadRegister():number {
         pins.i2cWriteNumber(i2cAddr, 0x07, NumberFormat.Int8LE)
-        let temp=pins.i2cReadNumber(i2cAddr, NumberFormat.Int16LE)
-        temp|=temp <<8
-        temp=temp*0.02-273.15
-        serial.writeNumber(temp)
+        let result = pins.i2cReadNumber(i2cAddr, NumberFormat.Int8LE)
+          serial.writeNumber(result)
+        //result |= result << 8
         return 0
     }
 }
